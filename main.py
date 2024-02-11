10e55d7db2013a3729131d5ce96f3d93103ed535
DEXE <77177483+DEX-E@users.noreply.github.com>
2024-02-11 15:48:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+import uvloop
+import sys
+
+if not os.getenv('DEVICE_ID') and \
+        not os.getenv('ACCOUNT_ID') and \
+        not os.getenv('SECRET'):
+    print("Please paste your device auths into the \".env\" file.\n"
+          "If you're confused, re-watch the tutorial.")
+    sys.exit()
+
+os.system('pip install -U SEKKAYBOT')
+os.system('clear')
+
+import FNBOT2
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+client = SEKKAYBOT.SekkayBot(
+    device_id=os.getenv('DEVICE_ID'),
+    account_id=os.getenv('ACCOUNT_ID'),
+    secret=os.getenv('SECRET')
+)
+
+try:
+    client.run()
+except Exception as e:
+    print(e)
+    print("Failed to login, your device auths are probably invalid")
