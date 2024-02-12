32fa853c5618639af4f6b6a09692b7dcb6dcfa8c
DEXE <77177483+DEX-E@users.noreply.github.com>
2024-02-12 22:00:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import asyncio
 import os
-import uvloop
 import sys
+
+
 
 if not os.getenv('DEVICE_ID') and \
         not os.getenv('ACCOUNT_ID') and \
         not os.getenv('SECRET'):
-    print("Please paste your device auths into the \".env\" file.\n"
-          "If you're confused, re-watch the tutorial.")
+    print("Please paste your device auths into the \".env\" file.\n")
     sys.exit()
 
-os.system('pip install -U SEKKAYBOT')
+os.system('python3 -m pip install --upgrade SEKKAYBOT ')
 os.system('clear')
 
 import SEKKAYBOT
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 client = SEKKAYBOT.SekkayBot(
     device_id=os.getenv('DEVICE_ID'),
     account_id=os.getenv('ACCOUNT_ID'),
     secret=os.getenv('SECRET')
 )
-
 try:
-    client.run()
+  client.run()
 except Exception as e:
-    print(e)
-    print("Failed to login, your device auths are probably invalid")
+  print(e)
+  print("Can't login because your device auths is probably wrong.")
+  print("are you sure you have put your IDs in secrets env or in .device  file ?")
+
